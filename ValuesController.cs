0e44c84bec07cac106c88fd12ff9f7c652341d4f
nikhilnimapinfo <166018854+nikhilnimapinfo@users.noreply.github.com>
2024-05-24 19:28:29 +0530

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+   // [Authorize]
+    public class ValuesController : ApiController
+    {
+        static List<string> Strings = new List<string>()
+        {
+         "value 0","value 1","value 2"
+        };
+
+        //GET api/values
+
+        public IEnumerable<string> Get()
+        {
+            return Strings;
+
+        }
+
+       // GET api/values/5
+        public string Get(int id)
+        {
+            
+                return Strings[id];
+           
+        }
+
+        // POST api/values
+       // [HttpPost]
+        //[Route()]
+        public void Post([FromBody]string value)
+        {
+            Strings.Add(value);
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            Strings[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Strings.RemoveAt(id);
+        }
+    }
+}
